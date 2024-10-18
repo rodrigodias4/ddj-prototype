45d0c7cca4f5115676b2a527cc497e45ba5671e1
franciscogf1904 <francisco.goncalves.ferreira@tecnico.ulisboa.pt>
2024-10-18 01:14:48 +0100

    Timer Final
    Yeehaw

--- Assets/Scripts/ScoreCalculation.cs
+++ Assets/Scripts/ScoreCalculation.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Timer;
 
 public class ScoreCalculation : MonoBehaviour
 {
     // Score is presented at the end of the game
     public static int score;
-    public static int dishCounter
+    public static int dishCounter;
     public static int customerKilled;
     public static int tips;
+    public TextMeshProUGUI scoreText;
+
+    public static int highScore = 0;
+
+    public Timer.Timer timer;
 
 
 
@@ -18,23 +25,36 @@ public class ScoreCalculation : MonoBehaviour
         dishCounter = 0;
         customerKilled = 0;
         tips = 0;
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     void Update()
     {
         // If the game is over, the score is calculated
-        if (Serve){
-            dishCounter++;
-        }
-        if (Kill){
-            customerKilled++;
-        }
-        if (Tip){
-            tips += tips.amount;
-        }
-        if (Timer.timeremaining <= 0)
+        if (timer.timeremaining <= 0)
         {
             score = dishCounter * 50 - customerKilled * 100 + tips;
+            scoreText.text = "Score: " + score;
+            scoreText.color = Color.yellow;
         }
+
+        if (score > highScore){
+            highScore = score;
+        }
+    }
+
+    public static void IncrementDishCounter()
+    {
+        dishCounter++;
+    }
+
+    public static void IncrementCustomerKilled()
+    {
+        customerKilled++;
+    }
+
+    public static void AddTips(int tipAmount)
+    {
+        tips += tipAmount;
     }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Timer: MonoBehaviour
+namespace Timer
 {
-    [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float timeremaining;
-
-    // Update is called once per frame
-    void Update()
+    public class Timer : MonoBehaviour
     {
-        if (timeremaining > 0)
-        {
-            timeremaining -= Time.deltaTime;
-        }
+        [SerializeField] TextMeshProUGUI timerText;
+        public float timeremaining { get; set; }
 
-        if (timeremaining <= 0)
+        // Update is called once per frame
+
+        void Start()
         {
-            timerText.text = "00:00";
-            timerText.color = Color.red;
-            return;
+            timeremaining = 10;
         }
-    
-        int minutes = Mathf.FloorToInt(timeremaining / 60);
-        int seconds = Mathf.FloorToInt(timeremaining % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        void Update()
+        {
+            if (timeremaining > 0)
+            {
+                timeremaining -= Time.deltaTime;
+            }
+
+            if (timeremaining <= 0)
+            {
+                timerText.text = "00:00";
+                timerText.color = Color.red;
+                return;
+            }
+
+            int minutes = Mathf.FloorToInt(timeremaining / 60);
+            int seconds = Mathf.FloorToInt(timeremaining % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
     }
+
 }
+
+
