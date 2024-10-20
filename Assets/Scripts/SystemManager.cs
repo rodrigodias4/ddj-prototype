eb18f9754ae77998e0ba95a8b778561adf91fcb5
franciscogf1904 <125879241+franciscogf1904@users.noreply.github.com>
2024-10-20 22:54:36 +0100

    System (#36)
    * Started Timer and Score
    
    These are not functional yet since they need things from the Customer
    
    * Timer Final
    
    Yeehaw
    
    * Finish Timer And Score
    
    Added Hi Score and centered it in the canvas
    
    * Small prefab fixes
    
    * Changed font & look, removed blackboard
    
    * Moved canvas changes to prefab
    
    * Score Fix
    
    Score fix hihihi
    
    * Tidied Up to Prefab storage, Added Black panel for visibility
    
    * Feat: Added Pause after Timer countdown ends
    
    * Integration
    
    * Integration 2
    
    Fixes in integration
    
    * Changed timer to 90s, added scene restart with esc
    
    ---------
    
    Co-authored-by: Rodrigo Dias <rodrigofdias@tecnico.ulisboa.pt>
    Co-authored-by: enderesting <datendza@gmail.com>

---
+++ Assets/Scripts/SystemManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SystemManager : MonoBehaviour
+{
+
+    // Check for the Escape key press to reload the scene
+    void Update()
+    {
+        // Press the "Escape" key to reload the scene
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReloadScene();
+        }
+    }
+    public void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
