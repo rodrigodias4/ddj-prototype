162ad17d3a4c0ef3d91fd9853f24ec07e160ecb8
Rodrigo Dias <rodrigofdias@tecnico.ulisboa.pt>
2024-10-21 05:26:15 +0100

    Added main menu and cursor

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scenes/MainGameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
